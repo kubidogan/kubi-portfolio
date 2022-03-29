e243d5a0e175fe868b01959a8fe6fb9e3ba52a62
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-03-29 13:24:25 +0100

    added linkedin and github icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import GRAD from '../../assets/grad.JPG'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,7 +11,8 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Kubi Dogan</h1>
         <h5 className="text-light">Fullstack Developer</h5>
-        <CTA/>
+        <CTA />
+        <HeaderSocials />
 
         <div className="me">
           <img src={GRAD} alt="my grad pic" />
