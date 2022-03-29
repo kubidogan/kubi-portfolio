92f8ad8aedafc6be3d520940576a374f18d88da1
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-03-29 14:56:03 +0100

    header socials done

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,9 +4,9 @@ import {BsGithub} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
-      <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com" target="_blank"><BsGithub/></a>
+    <div className='header__socials'>
+      <a href="https://www.linkedin.com/in/kubilay-dogan/" target="_blank"><BsLinkedin/></a>
+      <a href="https://github.com/kubidogan" target="_blank"><BsGithub/></a>
     </div>
   )
 }
