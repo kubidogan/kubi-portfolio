5bb635264b4c72b7dd8237b76d25fc675d0e8d53
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-03-29 13:13:32 +0100

    first push portfolio

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import GRAD from '../../assets/grad.JPG'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Kubi Dogan</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+
+        <div className="me">
+          <img src={GRAD} alt="my grad pic" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
